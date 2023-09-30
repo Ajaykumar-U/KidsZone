8f32449b45856549a2addb36bae7258979ff2d9c
Ajaykumar-U <ajaykumaru008@gmail.com>
2023-09-30 11:00:11 +0530

    Initial Setup Done

--- app/src/main/java/com/ajay/kidszone/HomeActivity.kt
+++ app/src/main/java/com/ajay/kidszone/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.ajay.kidszone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
